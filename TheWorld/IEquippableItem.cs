85c958dcf3dec96d2a8ec083d15f347e4fa3545b
mobesCodes <58530760+mobesCodes@users.noreply.github.com>
2020-04-09 00:51:16 -0400

    Added equippable item interface
    Added the IEquippableItem interface which has statschart Stats and bool IsEquipped

---
+++ TheWorld/IEquippableItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld
+{
+    /// <summary>
+    /// An item that can be equipped
+  
+    public interface IEquippableItem
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        public StatChart Stats // it has stats
+        {
+            get;
+            set;
+        }
+        bool IsEquipped // it can be equipped 
+        {
+            get;
+            set;
+        }
+
+        
+    }
+}

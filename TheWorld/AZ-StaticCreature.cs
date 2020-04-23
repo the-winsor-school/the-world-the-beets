fb4a992ae299c2cbd9939255b7a08e903a942555
axzhang21 <amelia.zhang@winsor.edu>
2020-04-23 09:26:19 -0400

    StaticCreatureShortcut

---
+++ TheWorld/AZ-StaticCreature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TheWorld
+{
+    public partial class Creature
+    {
+        //final boss "Big Rona"
+        public static Creature BigRona => new Creature()
+        {
+            Name = "Big Rona"
+            Description = "This is the final boss. Defeat Big Rona to win the game.",
+            Stats = new StatChart()
+            {
+                Atk = new Dice("1d20+5"),
+                Def = new Dice("1d12+10"),
+                Exp = 1000
+                Hps = 1000
+                Level = 100
+                Max
+            }
+        }
+
+
+    }
+}

b9e13de16471789c7ef56ba21972b7967e2e0d5c
mobesCodes <58530760+mobesCodes@users.noreply.github.com>
2020-05-04 13:25:27 -0400

    added empty class
    yeah

---
+++ TheWorld/EmptyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace TheWorld
+{
+    public class Backpack
+    {
+        public Backpack()
+        {
+
+        }
+    }
+}
+/// <summary>
+/// The items. In Stacks.  By Name.
+/// _______________________________________
+/// TODO: Hard Achievement
+/// Encapsulate this Backpack into a Specialized container class.
+/// The Backpack class should include additional properties such as:
+///
+/// int MaxCapacity  //how much weight can it hold?
+/// int CurrentWeight //Calculates the weight of all items currently in it
+/// bool Contains(string itemName)
+///
+/// void Add(ICarryable item, string uid)
+/// void Remove(string uid)
+///
+/// _______________________________________
+/// TODO: Hard Achievement (2)
+/// Add to the Backpack class
+///
+/// void Use(string uid)
+///
+/// such that, only usable items which are in the backpack can be used directly from the backpack.
+/// don't forget to handle events like ItemDepletedException.
+/// </summary>
